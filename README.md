64bfd9ba9e947ba473ff3685d4558a97b7d7fddc
GarrettChristian <garrettchristian2@gmail.com>
2020-12-10 17:59:39 -0500

    simple message server

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ a couple guides I'm looking at:
 > - http://zetcode.com/spring/websocket/
 > - https://developer.okta.com/blog/2019/10/09/java-spring-websocket-tutorial
 > - https://spring.io/blog/2015/10/13/react-js-and-spring-data-rest-part-4-events
+> - https://spring.io/guides/tutorials/react-and-spring-data-rest/
+> - https://helptechcommunity.wordpress.com/2020/01/28/websocket-chat-application-using-spring-boot-and-react-js/
+> - https://blog.cloudboost.io/simple-chat-react-java-6923b54d65a0
--- websock_demo2_client/src/App.js
+++ websock_demo2_client/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import SockJsClient from 'react-stomp';
+// import Button from 'react-bootstrap/Button';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          messages: [],
+          typedMessage: "Message To Send",
+          name: "Garrett"
+      }
+  }
+
+  setName = (name) => {
+      console.log(name);
+      this.setState({name: name});
+  };
+
+  sendMessage = () => {
+    this.clientRef.sendMessage('/app/chat', JSON.stringify({
+        from: this.state.name,
+        text: this.state.typedMessage
+    }));
+    this.setState({typedMessage: "Message To Send"});
+  };
+
+  displayMessages = () => {
+    return (
+      <div>
+          {this.state.messages.map(msg => {
+            return (
+                <div>
+                    {this.state.name === msg.from ?
+                      <div>
+                        <p style={{ color: 'green' }}>{msg.from} : {msg.text} : {msg.time}</p>
+                      </div> :
+                      <div>
+                        <p style={{ color: 'red' }}>{msg.from} : {msg.text} : {msg.time}</p>
+                      </div>
+                    }
+              </div>)
+          })}
+      </div>
+    );
+  };  
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            {this.state.name} Testing Web Sockets!
+          </h1>
+      
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              <p>User : </p>
+              <textarea value={this.state.name} onChange={(event) => {
+                this.setState({name: event.target.value});
+              }}/>
+            </label>
+          </form>
+
+          <div className="align-center">
+            <br/><br/>
+
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <p>Message : </p>
+                <textarea value={this.state.typedMessage} onChange={(event) => {
+                  this.setState({typedMessage: event.target.value});
+                }}/>
+              </label>
+            </form>
+
+            <button onClick={this.sendMessage}>Send</button>
+
+          </div>
+          <br/><br/>
+          <div className="align-center">
+              Messages:{this.displayMessages()}
+          </div>
+
+          <div>
+            <SockJsClient url='http://localhost:8080/chat/'
+              topics={['/topic/messages']}
+              onConnect={() => {
+                  console.log("connected!");
+              }}
+              onDisconnect={() => {
+                  console.log("Disconnected");
+              }}
+              onMessage={(msg) => {
+                  var jobs = this.state.messages;
+                  jobs.push(msg);
+                  this.setState({messages: jobs});
+                  console.log(msg);
+              }}
+              ref={(client) => {
+                  this.clientRef = client
+              }}/>
+          </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
